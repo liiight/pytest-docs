154f072d314685d4ced47770678eb71b4f0ee4d3
orcarmi <ocarmi@proofpoint.com>
2018-11-06 00:50:50 +0200

    fixed entry point

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="pytest-docs",
-    version="0.0.3",
+    version="0.0.4",
     author="Or Carmi",
     author_email="or.carmi82@gmail.com",
     maintainer="Or Carmi",
