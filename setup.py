5d2767c4a642adf9f4ec74743ff1b3f0fd6cdfe0
orcarmi <ocarmi@proofpoint.com>
2018-11-06 11:01:34 +0200

    version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="pytest-docs",
-    version="0.0.4",
+    version="0.0.5",
     author="Or Carmi",
     author_email="or.carmi82@gmail.com",
     maintainer="Or Carmi",
