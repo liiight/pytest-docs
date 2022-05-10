37660100d920ac38e323384106972cdb0743f281
Markus Anthony Dørheim Ho-Yen <96105947+markus-work@users.noreply.github.com>
2022-05-10 09:07:10 +0200

    Removed dummy formatter

--- tests/formatters_for_test/empty_formatter.py
+++ tests/formatters_for_test/empty_formatter.py
@@ -20,24 +20,3 @@ class CustomFormatter(Formatter):
     @staticmethod
     def marker_format(marker):
         return ""
-
-
-class DummyCustomFormatter(Formatter):
-    name = "md"
-    marker_prefix = "\n"
-
-    @staticmethod
-    def module_name_format(element):
-        return "# {}".format(element)
-
-    @staticmethod
-    def class_name_format(element):
-        return "## {}".format(element)
-
-    @staticmethod
-    def func_name_format(element):
-        return "### {}".format(element)
-
-    @staticmethod
-    def marker_format(marker):
-        return ""
